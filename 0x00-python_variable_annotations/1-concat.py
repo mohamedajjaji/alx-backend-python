debc77d99113bbf2eda7ffd9832f54d0cb8db8ca
Mohamed AJJAJI <majjaji98@gmail.com>
2024-03-15 01:32:08 +0000

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Task 1 module
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Type-Annotated String Concatenation Function
+    """
+    return str1 + str2
