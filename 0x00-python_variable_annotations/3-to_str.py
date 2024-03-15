eeca04eb6703c802307ef5b6810f802db0690f51
Mohamed AJJAJI <majjaji98@gmail.com>
2024-03-15 01:36:50 +0000

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Task 3 module
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Type-Annotated Floating-Point to String Conversion Function
+    """
+    return str(n)
