f8c1ace1b6c9f084f1f0e22f0d2396c9ecc78e22
Mohamed AJJAJI <majjaji98@gmail.com>
2024-03-15 01:27:28 +0000

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Task 0 module
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Type-Annotated Addition Function for Floating-Point Numbers
+    """
+    return a + b
