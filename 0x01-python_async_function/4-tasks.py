fc456ef2b2316ab4987df78ecfb31fe9adc49953
Mohamed AJJAJI <majjaji98@gmail.com>
2024-03-18 18:17:12 +0000

    Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Task 4 module
+"""
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronous Task Generation with task_wait_random in task_wait_n
+    """
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
